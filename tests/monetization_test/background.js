9c3e3932f2f99a35db7c63a2547a37752722d05b
Hemyl_qa <hemyl.qa@gmail.com>
2025-05-21 14:31:42 +0300

    update extensions

--- tests/monetization_test/background.js
+++ tests/monetization_test/background.js
@@ -1,79 +1,86 @@
-const domain = "https://dev.astralink.click";
+const domain = 'https://dev.astralink.click';
 
 const setDefaultConfig = async () => {
-  await chrome.storage.local.set({
-    activeWidget: true,
-    moduleList: [
-      "none",
-      "yahoo_dev",
-      "yahoo_stable",
-      "google_dev",
-      "google_stable",
-      "yt-pre-vid",
-    ],
-    yahoo_dev: {
-      url: `${domain}/mon-d/conf/ya?q=oy9isnkr`,
-      name: "Yahoo Dev",
-    },
-    yahoo_stable: {
-      url: `${domain}/mon-d/conf/ya-prod?q=oy9isnkr`,
-      name: "Yahoo Stable",
-    },
-    google_dev: {
-      url: `${domain}/mon-d/conf/go?q=oy9isnkr`,
-      name: "Google Dev",
-    },
-    google_stable: {
-      url: `${domain}/mon-d/conf/go-prod?q=oy9isnkr`,
-      name: "Google Stable",
-    },
-    "yt-pre-vid": {
-      url: `${domain}/mon-d/conf/yt-pre-vid?q=oy9isnkr`,
-      name: "YT preroll video",
-    },
-  });
-};
+    await chrome.storage.local.set({
+        activeWidget: true,
+        moduleList: [
+            'none', 
+            'yahoo_dev', 
+            'yahoo_stable', 
+            'google_dev', 
+            'google_test_performance',
+            'google_stable', 
+            'yt-pre-vid',
+            'yt-search-links-dev'
+        ],
+        'yt-search-links-dev': {
+            url: `${domain}/mon-d/conf/yt-search-links-dev?q=oy9isnkr`,
+            name: 'YT search links dev',
+        },
+        'yahoo_dev': {
+            url: `${domain}/mon-d/conf/ya?q=oy9isnkr`,
+            name: 'Yahoo Dev',
+        },
+        'yahoo_stable': {
+            url: `${domain}/mon-d/conf/ya-prod?q=oy9isnkr`,
+            name: 'Yahoo Stable',
+        },
+        'google_test_performance': {
+            url: `${domain}/mon-d/conf/go-perf?q=oy9isnkr`,
+            name: 'Google Test Performance',
+        },
+        'google_dev': {
+            url: `${domain}/mon-d/conf/go?q=oy9isnkr`,
+            name: 'Google Dev',
+        },
+        'google_stable': {
+            url: `${domain}/mon-d/conf/go-prod?q=oy9isnkr`,
+            name: 'Google Stable',
+        },
+        'yt-pre-vid': {
+            url: `${domain}/mon-d/conf/yt-pre-vid?q=oy9isnkr`,
+            name: 'YT preroll video',
+        },
+    });
+}
 
 const getConfig = async () => {
-  const { activeModule, configUrl } = await chrome.storage.local.get([
-    "activeModule",
-    "configUrl",
-  ]);
+    const {activeModule, configUrl} = await chrome.storage.local.get(['activeModule', 'configUrl']);
 
-  if (!configUrl || !activeModule || activeModule === "none") {
-    await chrome.storage.local.set({ mon: "" });
-    return;
-  }
+    if (!configUrl || !activeModule || activeModule === 'none') {
+        await chrome.storage.local.set({mon: ''});
+        return;
+    }
 
-  const response = await fetch(configUrl);
-  const data = await response.text();
+    const response = await fetch(configUrl);
+    const data = await response.text();
 
-  await chrome.storage.local.set({ mon: data });
-};
+    await chrome.storage.local.set({mon: data});
+}
 
 const updateConfigUrl = async (moduleName) => {
-  const moduleData = await chrome.storage.local.get(moduleName);
-  const url = moduleData[moduleName].url;
+    const moduleData = await chrome.storage.local.get(moduleName);
+    const url = moduleData[moduleName].url;
 
-  await chrome.storage.local.set({ configUrl: url });
+    await chrome.storage.local.set({configUrl: url});
 
-  if (url) {
-    getConfig();
-  }
-};
+    if (url) {
+        getConfig();
+    }
+}
 
 setDefaultConfig();
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.type === "UPDATE_CONFIG") {
-    await getConfig();
-  }
+    if (request.type === 'UPDATE_CONFIG') {
+        await getConfig();
+    }
 });
 
 chrome.storage.onChanged.addListener((changes, areaName) => {
-  if (areaName === "local" && changes.activeModule) {
-    const activeModule = changes.activeModule.newValue;
+    if (areaName === 'local' && changes.activeModule) {
+        const activeModule = changes.activeModule.newValue;
 
-    updateConfigUrl(activeModule);
-  }
+        updateConfigUrl(activeModule);
+    }
 });
--- tests/monetization_test/content.js
+++ tests/monetization_test/content.js
@@ -1,31 +1,28 @@
 async function getOption() {
-  const { moduleList } = await chrome.storage.local.get("moduleList");
-  let result = "";
+    const { moduleList } = await chrome.storage.local.get('moduleList');
+    let result = "";
 
-  for (const module of moduleList) {
-    if (module === "none") {
-      result += `<option value="${module}">Select a module</option>`;
-      continue;
+    for (const module of moduleList) {
+        if (module === 'none') {
+            result += `<option value="${module}">Select a module</option>`;
+            continue;
+        }
+
+        const moduleData = await chrome.storage.local.get(module);
+
+        result += `<option value="${module}">${moduleData[module].name}</option>`;
     }
 
-    const moduleData = await chrome.storage.local.get(module);
-
-    result += `<option value="${module}">${moduleData[module].name}</option>`;
-  }
-
-  return result;
+    return result;
 }
 
 async function addWidget() {
-  const { activeWidget } = await chrome.storage.local.get("activeWidget");
-  const widget = document.createElement("div");
+    const { activeWidget } = await chrome.storage.local.get('activeWidget');
+    const widget = document.createElement('div');
 
-  widget.id = "dev-mon-widget";
-  widget.setAttribute(
-    "style",
-    "position: fixed; bottom: 20px; right: 20px; z-index: 99999999; background-color: #fff; padding: 10px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);",
-  );
-  widget.innerHTML = `
+    widget.id = 'dev-mon-widget';
+    widget.setAttribute('style', 'position: fixed; bottom: 20px; right: 20px; z-index: 99999999; background-color: #fff; padding: 10px; border-radius: 5px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);');
+    widget.innerHTML = `
         <form class="form-inline">
             <label class="my-1 mr-2" for="inlineFormCustomSelectPref">Active module</label>
             <select class="custom-select my-1 mr-sm-2" id="select-field">
@@ -34,65 +31,65 @@ async function addWidget() {
         </form>
     `;
 
-  if (!activeWidget || window.top !== window.self) {
-    widget.style.opacity = "0";
-    widget.style.pointerEvents = "none";
-  }
-
-  const select = widget.querySelector("#select-field");
-
-  select.addEventListener("change", async (event) => {
-    const activeModule = event.target.value;
-    await chrome.storage.local.set({ activeModule });
-  });
-
-  document.body.appendChild(widget);
-
-  chrome.storage.local.get("activeModule", ({ activeModule }) => {
-    if (!activeModule) {
-      select.value = "none";
-      return;
+    if (!activeWidget || window.top !== window.self) {
+        widget.style.opacity = '0';
+        widget.style.pointerEvents = 'none';
     }
 
-    select.value = activeModule;
-  });
+    const select = widget.querySelector('#select-field');
 
-  chrome.storage.onChanged.addListener((changes, areaName) => {
-    if (areaName === "local" && changes.activeWidget) {
-      const activeWidget = changes.activeWidget.newValue;
+    select.addEventListener('change', async (event) => {
+        const activeModule = event.target.value;
+        await chrome.storage.local.set({activeModule});
+    });
 
-      if (window.top !== window.self) {
-        return;
-      }
+    document.body.appendChild(widget);
 
-      if (activeWidget) {
-        widget.style.opacity = "1";
-        widget.style.pointerEvents = "auto";
-      } else {
-        widget.style.opacity = "0";
-        widget.style.pointerEvents = "none";
-      }
-    }
-  });
+    chrome.storage.local.get('activeModule', ({activeModule}) => {
+        if (!activeModule) {
+            select.value = 'none';
+            return;
+        }
+    
+        select.value = activeModule;
+    })
+
+    chrome.storage.onChanged.addListener((changes, areaName) => {
+        if (areaName === 'local' && changes.activeWidget) {
+            const activeWidget = changes.activeWidget.newValue;
+
+            if (window.top !== window.self) {
+                return;
+            }
+
+            if (activeWidget) {
+                widget.style.opacity = '1';
+                widget.style.pointerEvents = 'auto';
+            } else {
+                widget.style.opacity = '0';
+                widget.style.pointerEvents = 'none';
+            }
+        }
+    });
 }
 
 async function start() {
-  if (!document.body) {
-    setTimeout(start, 100);
-    return;
-  }
+    if (!document.body) {
+        setTimeout(start, 100);
+        return;
+    }
 
-  addWidget();
+    addWidget();
 
-  const html = await chrome.storage.local.get("mon");
+    const html = await chrome.storage.local.get('mon');
 
-  if (html.mon) {
-    document.body.insertAdjacentHTML("beforeend", html.mon);
+    if (html.mon) {
+        document.body.insertAdjacentHTML('beforeend', html.mon);
 
-    chrome.runtime.sendMessage({
-      type: "UPDATE_CONFIG",
-    });
-  }
+        // chrome.runtime.sendMessage({
+        //     type: 'UPDATE_CONFIG'
+        // });
+    }
 }
 
 start();
